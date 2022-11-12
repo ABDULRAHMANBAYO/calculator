0c7014e320a372196bcd3a88b72760d084b334e3
abdulrahmanbayo <ramhziworld@gmail.com>
2022-11-12 17:48:37 +0100

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Kole-ibrahim Abdul rahman on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
